49879ac1daaeb3f8fe112cf2986140bdedcdad1e
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2020-11-17 18:35:26 +0100

    commit with existing project

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 /** @format */
+document.addEventListener("DOMContentLoaded", function () {
+  const nav = document.querySelector(".navbar");
+
+  function addShadow() {
+    if (window.scrollY >= 300) {
+      nav.classList.add("shadow-bg");
+    } else {
+      nav.classList.remove("shadow-bg");
+    }
+  }
+  window.addEventListener("scroll", addShadow);
+
+  $(document).click(function (event) {
+    const clickover = $(event.target);
+    const _opened = $(".navbar-collapse").hasClass("show");
+    if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+      $(".navbar-toggler").click();
+    }
+  });
+});
